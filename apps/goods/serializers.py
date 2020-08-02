0f7b9c64c11a81dff12d54f6c92691ca07e53828
qqfh <ljh_132s@qq.com>
2020-08-02 23:52:56 +0800

    学习了APIView的POST保存，ModelSerializer序列化模型

--- apps/goods/serializers.py
+++ apps/goods/serializers.py
@@ -1,6 +1,20 @@
 from rest_framework import serializers
 
-class GoodsSerializer(serializers.Serializer):
-    name = serializers.CharField(required=True,max_length=100)
-    click_num = serializers.IntegerField(default=0)
-    goods_front_image = serializers.ImageField()
+from goods.models import Goods,GoodsCategory
+
+# class GoodsSerializer(serializers.Serializer):
+#     name = serializers.CharField(required=True,max_length=100)
+#     click_num = serializers.IntegerField(default=0)
+
+class GoodsCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = GoodsCategory
+        fields = "__all__"
+
+class GoodsSerializer(serializers.ModelSerializer):
+    category = GoodsCategorySerializer()
+    class Meta:
+        model = Goods
+        # fields = ('name','click_num','id')  # 取出部分字段
+        fields = "__all__"
+
--- apps/goods/views.py
+++ apps/goods/views.py
@@ -2,12 +2,20 @@
 from .serializers import GoodsSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 from .models import Goods
 
 class GoodstListView(APIView):
     """列表页"""
     def get(self,request,format=None):
-        all_goods = Goods.objects.all()[:10]
+        all_goods = Goods.objects.all()
         goods_serializer = GoodsSerializer(all_goods,many=True)
         return Response(goods_serializer.data)
+
+    def post(self, request, format=None):
+        serializer = GoodsSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
